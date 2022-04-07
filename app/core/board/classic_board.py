110bfe48b7f39ee82ea6b2ec8bd738be101359bc
Jung-JongHyuk <system97.solar@gmail.com>
2022-04-07 11:50:20 +0900

    classic board의 생성자, getter & setter 구현

--- app/core/board/classic_board.py
+++ app/core/board/classic_board.py
@@ -1,12 +1,19 @@
 from typing import Any
 
 from core.board.board import Board
+from core.board.cell import Cell
 from core.board_position.plane_grid_board_position import PlaneGridBoardPosition
 
 
 class ClassicBoard(Board):
+    def __init__(self):
+        self.rowSize = 9
+        self.colSize = 9
+        self.boxSize = 3
+        self.board = [[Cell(value=None, is_hint=False)] * self.colSize] * self.rowSize
+
     def set_value(self, position: PlaneGridBoardPosition, value: Any) -> None:
-        pass
+        self.board[position.rowIndex][position.colIndex] = value
 
     def read_from_json(self, json: str) -> None:
         pass
@@ -15,4 +22,4 @@ class ClassicBoard(Board):
         pass
 
     def get_value(self, position: PlaneGridBoardPosition) -> Any:
-        pass
+        return self.board[position.rowIndex][position.colIndex]
