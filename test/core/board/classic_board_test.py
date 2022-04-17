32bc8c4a86166f574d031e9d3461f6d1f3a492c8
Jung-JongHyuk <system97.solar@gmail.com>
2022-04-17 16:21:29 +0900

    테스트 코드 추가

--- test/core/board/classic_board_test.py
+++ test/core/board/classic_board_test.py
@@ -1,4 +1,6 @@
 import unittest
+
+from core.board.cell import Cell
 from core.board.classic_board import ClassicBoard
 from core.board_position.plane_grid_board_position import PlaneGridBoardPosition
 
@@ -15,7 +17,7 @@ class ClassicBoardTestCase(unittest.TestCase):
         board = ClassicBoard()
         position = PlaneGridBoardPosition(rowIndex=3, colIndex=3)
 
-        value_to_set = 7
+        value_to_set = Cell(value=7, is_hint=False)
         board.set_value(position=position, value=value_to_set)
         value = board.get_value(position=position)
 
@@ -25,10 +27,20 @@ class ClassicBoardTestCase(unittest.TestCase):
         board = ClassicBoard()
         position = PlaneGridBoardPosition(rowIndex=-1, colIndex=10)
 
-        value_to_set = 7
-        board.set_value(position=position, value=value_to_set)
-        self.assertRaises(IndexError)
+        with self.assertRaises(IndexError):
+            value_to_set = Cell(value=7, is_hint=False)
+            board.set_value(position=position, value=value_to_set)
 
+    def test_json(self):
+        board = ClassicBoard()
+        json = board.write_to_json()
+
+        board.read_from_json(json)
+
+        board_row_size = len(board.board)
+        board_col_size = len(board.board[0])
+        self.assertEqual(board_row_size, 9)
+        self.assertEqual(board_col_size, 9)
 
 
 if __name__ == '__main__':
