e87d79949ae3210d28ea93dd4f64e1d1457d510a
Jung-JongHyuk <system97.solar@gmail.com>
2022-05-24 11:33:31 +0900

    classic board의 테스트코드 추가 및 리팩토링

--- test/core/board/classic_board_test.py
+++ test/core/board/classic_board_test.py
@@ -23,15 +23,36 @@ class ClassicBoardTestCase(unittest.TestCase):
 
         self.assertEqual(value, value_to_set)
 
-    def test_accessor_when_invalid_position_value(self):
-        with self.assertRaises(IndexError):
-            value_to_set = Cell(value=7, is_hint=False)
-            position = PlaneGridBoardPosition(rowIndex=-1, colIndex=10)
-            self.board.set_value(position=position, cell=value_to_set)
+    def test_accessor_immutability(self):
+        position = PlaneGridBoardPosition(rowIndex=3, colIndex=3)
+
+        value = self.board.get_value(position=position)
+        value.value = 100
+        self.assertNotEqual(value, self.board.get_value(position=position))
+
+        value_to_set = Cell(value=7, is_hint=False)
+        self.board.set_value(position=position, cell=value_to_set)
+        value_to_set.value = 100
+        self.assertNotEqual(value_to_set, self.board.get_value(position=position))
+
+    def test_accessor_when_invalid_value(self):
+        position = PlaneGridBoardPosition(rowIndex=3, colIndex=3)
+
+        value_to_set = Cell(value=None, is_hint=False)
+        self.board.set_value(position=position, cell=value_to_set)
+        value = self.board.get_value(position=position)
+
+        self.assertEqual(value, value_to_set)
 
         with self.assertRaises(ValueError):
             value_to_set = Cell(value=10, is_hint=False)
             position = PlaneGridBoardPosition(rowIndex=3, colIndex=3)
+            self.board.set_value(position=position, cell=value_to_set)
+
+    def test_accessor_when_invalid_position_value(self):
+        with self.assertRaises(IndexError):
+            value_to_set = Cell(value=7, is_hint=False)
+            position = PlaneGridBoardPosition(rowIndex=-1, colIndex=10)
             self.board.set_value(position=position, cell=value_to_set)
 
     def test_json_conversion(self):
